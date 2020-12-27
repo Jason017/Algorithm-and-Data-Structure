44c75a72fb022ac662e90422bc2bfaf2ede7ef29
gjason <46449309+Jason017@users.noreply.github.com>
2020-12-27 04:15:28 -0500

    Add files via upload

---
+++ LeetCode/TwoSum.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> num_map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (num_map.containsKey(target - nums[i])) {
+                return new int[]{num_map.get(target - nums[i]), i};
+            }
+            num_map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No match found");
+    }
+}
