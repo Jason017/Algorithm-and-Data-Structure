1f92578f412b3745722dcdbf9d6ba63e36604316
gjason <46449309+Jason017@users.noreply.github.com>
2020-12-27 03:19:46 -0500

    Add files via upload

---
+++ HashMapPractice.java
@@ -0,0 +1,24 @@
+package Example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapPractice {
+    public static void main(String[] args) {
+        String[] strings = {"a", "b", "a", "f", "b", "a", "z"};
+        System.out.println(wordCount(strings));
+    }
+
+    private static Map<String, Integer> wordCount(String[] strings) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String s : strings) {
+            if (!map.containsKey(s))
+                map.put(s, 1);
+            else {
+                int count = map.get(s);
+                map.put(s, count++);
+            }
+        }
+        return map;
+    }
+}
