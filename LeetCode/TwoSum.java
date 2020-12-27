98516318cc01c93fc0469d7ae566a2bef61e44ac
gjason <46449309+Jason017@users.noreply.github.com>
2020-12-27 04:17:48 -0500

    Update TwoSum.java

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -5,12 +5,11 @@ import java.util.Map;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> num_map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (num_map.containsKey(target - nums[i])) {
-                return new int[]{num_map.get(target - nums[i]), i};
-            }
-            num_map.put(nums[i], i);
+            if (map.containsKey(target - nums[i]))
+                return new int[]{map.get(target - nums[i]), i};
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No match found");
     }
