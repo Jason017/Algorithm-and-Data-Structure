fd76e4d024107623991cf65f959df6083e6eaf74
gjason <46449309+Jason017@users.noreply.github.com>
2021-01-22 15:08:41 -0500

    Update EasyQuestionPractice.java

--- LeetCode/EasyQuestionPractice.java
+++ LeetCode/EasyQuestionPractice.java
@@ -12,6 +12,26 @@ class Solution {
     }
 }
 
+// 9. Palindrome Number
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+}
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+        int r = 0;
+        while (x > r) {
+            r = r * 10 + x % 10;
+            x /= 10;
+        }
+        return x == r || x == r / 10;
+    }
+}
+
 // 206. Reverse Linked List
 /**
  * Definition for singly-linked list.
