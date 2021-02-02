584da90057f2759ad409fe0548c3f5c7b67073bc
gjason <46449309+Jason017@users.noreply.github.com>
2021-02-01 23:58:24 -0500

    Update EasyQuestionPractice.java

--- LeetCode/EasyQuestionPractice.java
+++ LeetCode/EasyQuestionPractice.java
@@ -32,6 +32,20 @@ class Solution {
     }
 }
 
+// 191. Number of 1 Bits
+class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int bits = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++){
+            if ((n & mask) != 0) bits++;
+            mask <<= 1;
+        }
+        return bits;
+    }
+}
+
 // 206. Reverse Linked List
 /**
  * Definition for singly-linked list.
