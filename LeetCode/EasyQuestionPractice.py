284651ca54619b29b9b4f8c955ddb4b5a2503622
Jason <jasonguan0107@gmail.com>
2021-03-11 23:59:54 -0500

    update

--- LeetCode/EasyQuestionPractice.py
+++ LeetCode/EasyQuestionPractice.py
@@ -1,4 +1,11 @@
 # 9. Palindrome Number
-Class Solution:
+class Solution:
 	def isPalindrome(self, x: int) -> bool:
 		return str(x) == str(x)[::-1]
+
+# 1332. Remove Palindromic Subsequences
+class Solution:
+	def removePalindromeSub(self, s: str) -> int:
+		if not s: 
+			return -1
+		return 1 if s[::-1] == s else 2
