6f5f19731c7d53195c8073d6d83cdd2e285a89bf
Jason <jasonguan0107@gmail.com>
2021-03-14 23:59:33 -0400

    314
    git commit -m 314

--- LeetCode/March_Leetcoding_Challenge.py
+++ LeetCode/March_Leetcoding_Challenge.py
@@ -72,3 +72,20 @@ class Solution:
                 num -= stack[-1]
                 ans.append(dct[stack[-1]])
         return "".join(map(str, ans))
+
+
+### March 14th: Swapping Nodes in a Linked List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:        
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        A, B = head, head
+        for i in range(1, k): A = A.next
+        nodeK, A = A, A.next
+        while A: A, B = A.next, B.next
+        nodeK.val, B.val = B.val, nodeK.val
+        return head     
