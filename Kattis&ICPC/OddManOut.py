1d9fb1b6da444cb1306d6083e70586d2394a968b
Jason <jasonguan0107@gmail.com>
2021-07-23 00:05:51 -0400

    Update

--- Kattis&ICPC/OddManOut.py
+++
@@ -1,8 +0,0 @@
-import collections
-
-N=int(input())
-for i in range(N):
-    G=input()
-    nums=[int(j) for j in input().split(' ')]
-    invitations=collections.Counter(nums)
-    print('Case #'+str(i+1)+': '+str(invitations.most_common()[len(invitations)-1][0]))
