7c4762d43542ff721c17202bf626a5506bde7eff
Jason017 <jasonguan0107@gmail.com>
2021-09-18 20:16:18 -0400

    bst

--- Data Structure/BST.py
+++ Data Structure/BST.py
@@ -1,10 +1,10 @@
 class Node:
-    def __init__(self, key) -> None:
+    def __init__(self, key):
         self.left = None
         self.right = None
         self.val = key
 
-def insert(root, key) -> int:
+def insert(root, key):
     if root:
         if root.val < key:
             root.right = insert(root.right, key)
@@ -31,26 +31,33 @@ r = insert(r, 70)
 r = insert(r, 60)
 r = insert(r, 80)
 
-lst = str(inorder(r))
-print()
+# lst = str(inorder(r))
 
 
-
-# Binary Search
-def binarySearch(arr, low, high, target):
-    if high >= low:
-        mid = (high+low) // 2
+# Binary Search recursively
+def binary_search_recursive(arr, start, end, target):
+    if start <= end:
+        mid = (start + end) // 2
         if arr[mid] == target:
             return mid
         elif arr[mid] > target:
-            return binarySearch(arr, low, mid-1, target)
+            return binary_search_recursive(arr, start, mid-1, target)
         elif arr[mid] < target:
-            return binarySearch(arr, mid+1, high, target)
+            return binary_search_recursive(arr, mid+1, end, target)
     return -1
 
-arr = [2,3,4,10,40,50]
-idx = binarySearch(arr, 0, len(arr)-1, 30)
-print(idx)
+# Binary Search iteratively
+def binary_search_iterative(arr, target):
+    mid, start, end = 0, 0, len(arr)
+    while start<=end:
+        mid = (start+end) // 2
+        if target == arr[mid]:
+            return mid
+        elif target < arr[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
 
 # Get the maximum height/depth of a binary tree
 def maxHeight(node):
@@ -58,3 +65,8 @@ def maxHeight(node):
         return 0
     else:
         return 1 + max(maxHeight(node.left), maxHeight(node.right)) 
+
+
+arr = [2,3,4,10,40,50]
+print(binary_search_recursive(arr, 0, len(arr)-1, 40))
+print(binary_search_iterative(arr, 40))
