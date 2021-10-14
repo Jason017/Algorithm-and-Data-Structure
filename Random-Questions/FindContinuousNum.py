aad4c5704359f7ef15469ef26244747af60a86cc
Jason <jasonguan0107@gmail.com>
2021-10-13 21:57:32 -0400

    10/13

---
+++ Random-Questions/FindContinuousNum.py
@@ -0,0 +1,46 @@
+def findContinuousNum(nums):
+    r = []
+    right = left = 0
+    for i in nums + [float('inf')]:
+        if i-1 == right:
+            right += 1
+        else:
+            if right:
+                if left < right:
+                    # r.append('%s-%s' % (left, right))
+                    r.append(list(range(left, right+1)))
+                else:
+                    # r.append(str(right))
+                    r.append([right])
+            left = right = i
+    # return '(%s)' % ', '.join(r)
+    return len(r)
+
+def consecutiveSegments(nums):
+    n = len(nums)
+    if n <= 1:
+        return n
+
+    x = nums[0]
+    ans = 1
+    for i in range(1, n):
+        if nums[i] != x+1:
+            ans += 1
+        x = nums[i]
+    return ans
+
+nums1 = [2, 3, 4, 5, 20, 13, 14, 15, 16, 17, 20]
+nums2 = [-4,-3,-1,3,4,7,8]
+nums3 = [17,20,34,45,46]
+nums4 = [4,6]
+print(findContinuousNum(nums1))
+print(findContinuousNum(nums2))
+print(findContinuousNum(nums3))
+print(findContinuousNum(nums4))
+
+print(consecutiveSegments(nums1))
+print(consecutiveSegments(nums2))
+print(consecutiveSegments(nums3))
+print(consecutiveSegments(nums4))
+
+
--- Random-Questions/FindTotalWays.py
+++ Random-Questions/FindTotalWays.py
@@ -1,4 +1,9 @@
 def findTotalWays(arr, i, k): 
+    '''
+    Given an integer array, find number of ways 
+    to calculate a target number using only array 
+    elements and addition or subtraction operator.
+    '''
     if (k == 0 and i == len(arr)): 
         return 1
     if (i >= len(arr)):
@@ -7,8 +12,8 @@ def findTotalWays(arr, i, k):
             findTotalWays(arr, i + 1, k - arr[i]) + 
             findTotalWays(arr, i + 1, k + arr[i])) 
 
-arr = [-3, 1, 3, 5, 7]
-k = 6
+arr1 = [-3, 1, 3, 5, 7], k1 = 6
+arr2 = [2, 3, -4, 4], k2 = 5
 
-print(findTotalWays(arr, 0, k))
-
+print(findTotalWays(arr1, 0, k1))
+print(findTotalWays(arr2, 0, k2))
