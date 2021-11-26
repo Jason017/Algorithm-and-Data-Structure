6ffe0041c3ddceac4970ef4387a10c68e4c91521
Jason <jasonguan0107@gmail.com>
2021-11-25 19:37:07 -0500

    first & last occurrence

--- Random-Questions/findFirstAndLastOccurrence.py
+++ Random-Questions/findFirstAndLastOccurrence.py
@@ -33,8 +33,44 @@ target=2
 print(first(arr, 0, len(arr)-1, target))
 print(last(arr, 0, len(arr)-1, target))
 
+
+
 # Solution 2: Iterative Implementation of Binary Search Solution 
 # O(log(n)) O(1)
 
+def first(arr, target):
+    low, high = 0, len(arr)-1
+    res = -1
+     
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] > target:
+            high = mid - 1
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            res = mid
+            high = mid - 1
+    return res
 
+def last(arr, target):
+    low, high = 0, len(arr)-1
+    res = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] > target:
+            high = mid - 1
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            res = mid
+            low = mid + 1
+    return res
+
+
+arr=[1,2,2,2,2,3,4,7,8,8]
+target=2
+print(first(arr, target))
+print(last(arr, target))
 
