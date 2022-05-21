ac1988f1294a41e9078ec95ad44ea77ba6709242
Jason017 <jasonguan0107@gmail.com>
2022-05-21 08:15:29 -0400

    java algo

--- algorithm/Java/Permutations.java
+++ algorithm/Java/Permutations.java
@@ -10,15 +10,15 @@ class Permutations {
     private static void dfs(int[] nums, int level, List<List<Integer>> output) {
         if (level == nums.length) {
             List<Integer> ans = new ArrayList<>();
-            for (int i=0; i<nums.length; i++) {
+            for (int i = 0; i < nums.length; i++) {
                 ans.add(nums[i]);
             }
             output.add(ans);
         }
 
-        for (int i=level; i<nums.length; i++) {
+        for (int i = level; i < nums.length; i++) {
             swap(nums, i, level);
-            dfs(nums, level+1, output);
+            dfs(nums, level + 1, output);
             swap(nums, i, level);
         }
     }
@@ -30,7 +30,7 @@ class Permutations {
     }
 
     public static void main(String args[]) {
-        int[] nums = new int[]{1,2,3}; 
+        int[] nums = new int[] { 1, 2, 3 };
         System.out.println(permute(nums));
     }
 }
